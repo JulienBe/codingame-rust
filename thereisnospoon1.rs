128b7a7ca972c0c0ace56b7bb0d1b25fcd7cdd1b
JulienBe <bertozzijulien@gmail.com>
2020-07-06 20:41:23 +0000

    thereisnospoon1_base

---
+++ thereisnospoon1.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+macro_rules! print_err {
+    ($($arg:tt)*) => (
+        {
+            use std::io::Write;
+            writeln!(&mut ::std::io::stderr(), $($arg)*).ok();
+        }
+    )
+}
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let width = parse_input!(input_line, i32); // the number of cells on the X axis
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let height = parse_input!(input_line, i32); // the number of cells on the Y axis
+    
+    let mut lines = Vec::new();
+    for i in 0..height as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let line = input_line.trim_right().to_string(); // width characters, each either 0 or .
+        lines.push(line);
+    }
+}
