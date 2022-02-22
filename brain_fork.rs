d67fedd9bf00c9ae3379300807b2025bfeb88a32
Bertozzi Julien <bertozzijulien@gmail.com>
2022-02-22 21:00:27 +0100

    brain_work v0.5

--- brain_fork.rs
+++ brain_fork.rs
@@ -1,42 +1,95 @@
 use std::io;
+use std::time::Instant;
 
+const POSSIBILITIES_TO_CONSIDER: usize = 4;
 const A: isize = 97;
-const NB_ZONES: isize = 30;
-const NB_RUNES: isize = 27;
+const NB_ZONES: usize = 30;
+const NB_RUNES: usize = 27;
+const RUNE_UP: Roll =   Roll { c: '+', };
+const RUNE_DOWN: Roll = Roll { c: '-', };
+const ZONE_UP: Roll =   Roll { c: '>', };
+const ZONE_DOWN: Roll = Roll { c: '<', };
 
-struct Roll<'a> {
-  inc: isize,
-  c: &'a str,
+#[derive(Debug)]
+struct Roll {
+  c: char,
 }
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct Ring {
   top: isize,
-  current: isize,
+  current: isize,  
 }
 impl Ring {
-  // in practice, only roll by +-1
-  fn roll(&mut self, roll: &Roll) {
-    if self.current == 0 && roll.inc < 0 {
-      self.current = self.top;
-    } else if self.current == self.top && roll.inc > 0 {
-      self.current = 0;
+  fn set_to<'a>(&mut self, target: isize, roll_down: &'a Roll, roll_up: &'a Roll) -> Move<'a> {
+    let mut dist_down = (self.current - target, roll_down);
+    let mut dist_up = (target - self.current, roll_up);    
+    if dist_down.0 < 0 {
+      dist_down.0 += self.top;
+    }
+    if dist_up.0 < 0 {
+        dist_up.0 += self.top;
+    }    
+    if dist_up.0 < dist_down.0 {
+      Move {
+        times: dist_up.0 as usize,
+        roll: roll_up,  
+      }  
+      //dist_up.1.c.repeat(dist_up.0 as usize)
     } else {
-      self.current += roll.inc;
+      Move {
+        times: dist_down.0 as usize,
+        roll: roll_down,  
+      }  
+      //dist_down.1.c.repeat(dist_down.0 as usize)
     }
   }
-
-  fn set_to(&mut self, target: isize, roll_down: &Roll, roll_up: &Roll) -> String {
-    let roll: &Roll = if target - self.current <= self.top / 2 {
-      roll_up
-    } else {
-      roll_down
-    };
-    let mut accum: String = String::new();
-    while target != self.current {
-      self.roll(roll);
-      accum.push_str(roll.c);
-    }
-    accum
+}
+#[derive(Copy, Clone, Debug)]
+struct Move<'m> {
+  times: usize,
+  roll: &'m Roll,
+}
+#[derive(Copy, Clone, Debug)]
+struct Terrain {
+  runes: [Ring; NB_ZONES],
+  zones: Ring,
+}
+impl Terrain {
+  fn new() -> Terrain {
+    Terrain {
+      runes: [Ring { top: (NB_RUNES as isize), current: 0 }; NB_ZONES],
+      zones: Ring { top: NB_ZONES as isize, current: 0},
+    }  
+  }
+}
+#[derive(Copy, Clone, Debug)]
+struct Transition<'m> {
+  rune_index: usize,
+  letter_i: usize,
+  zone_move: Move<'m>,  
+  rune_move: Move<'m>,
+}
+#[derive(Debug)]
+struct LookAhead<'m> {
+  seed: &'m Transition<'m>,
+  terrains_costs: Vec<(Terrain, usize)>,
+}
+impl LookAhead<'_> {
+  fn new<'m>(seed: &'m Transition, base: Terrain) -> LookAhead<'m> {
+    LookAhead {
+      seed,
+      terrains_costs: vec![(base, seed.score())],
+    }  
+  }
+}
+impl Transition<'_> {
+  fn score(&self) -> usize {
+    self.zone_move.times + self.rune_move.times  
+  }
+  fn to_string(&self) -> String {
+    let mut z = self.zone_move.roll.c.to_string().repeat(self.zone_move.times);             // that .to_string() heres bothers me. Maybe move rolls outside of const
+    z.push_str(self.rune_move.roll.c.to_string().repeat(self.rune_move.times).as_str());
+    z
   }
 }
 
@@ -47,42 +100,94 @@ fn main() {
   let mut index = 0;
   let mut target_string: String = String::new();
   eprintln!("input : {}", magic_phrase);
-  let rune_up =   Roll { inc: 1,  c: "+",  };
-  let rune_down = Roll { inc: -1, c: "-",  };
-  let zone_up =   Roll { inc: 1,  c: ">",  };
-  let zone_down = Roll { inc: -1, c: "<",  };
 
-  let mut zones: [Ring; NB_ZONES as usize] = [Ring { top: (NB_RUNES - 1), current: 0 }; NB_ZONES as usize];
-  let mut zone_ring = Ring { top: NB_ZONES - 1, current: 0 };
-
+  let mut terrain = Terrain::new();  
+   
   // ITERATE OVER EACH CHARACTER
+  let start = Instant::now();
   while index < magic_phrase.chars().count() {
-    let current_char: isize = magic_phrase.chars().nth(index).unwrap().to_ascii_lowercase() as isize;
-    let desired_rune_index = (current_char - (A - 1)).clamp(0, 255); // 0 to clamp spaces. 255 to avoid masking an error     
-
-    // brute force all possibilities. Compute would be better
-    let strs: Vec<(usize, String)> = zones.iter().enumerate().map(|(i, z)| {
-      let mut accum: String = String::new();
-      let zones_str = zone_ring.clone().set_to(i as isize, &zone_down, &zone_up);
-      let rune_str = z.clone().set_to(desired_rune_index, &rune_down, &rune_up);
-      accum.push_str(zones_str.as_str());
-      accum.push_str(rune_str.as_str());
-      (i, accum)
-    }).collect();
-    // pick min
-    let min: &(usize, String) = strs.iter()
-      .min_by(|x, y| x.1.chars().count().cmp(&y.1.chars().count()))
-      .unwrap();
-    zone_ring.current = min.0 as isize;
-    zones[zone_ring.current as usize].current = desired_rune_index;
-    // update final str
-    target_string.push_str(min.1.as_str());
+    let mvt = get_move(index, &magic_phrase, terrain);
+    terrain = mvt.1;
+    target_string.push_str(mvt.0.as_str());
     target_string.push_str(".");
-
     index += 1;
   }
 
-  // Write an action using println!("message...");
-  // To debug: eprintln!("Debug message...");    
+  eprintln!("Time: {:?}", start.elapsed());
   println!("{}", target_string);
+}
+
+fn get_move(phrase_index: usize, phrase: &String, terrain: Terrain) -> (String, Terrain) {
+  let letter_i = get_char_index_at(phrase_index, &phrase);
+  let seeds = best_transitions(&terrain, letter_i);  
+  
+  let mut look_ahead: Vec<LookAhead> = seeds.iter()
+    .map(|seed| LookAhead::new(seed, terrain))
+    .collect();
+  
+  /* That needs work 
+  // depth
+  if phrase_index % 5 == 0 {
+    for n in 1..100 {
+      if phrase_index + n < phrase.char_indices().count() {
+        let letter_i_plus = get_char_index_at(phrase_index + n, &phrase);
+        // each path
+        look_ahead.iter_mut().for_each(|mut seed| {
+          let mut next_tc = Vec::new();        
+          // branch to next paths
+          seed.terrains_costs.iter().for_each(|terr_cost| {
+            let transitions = best_transitions(&terr_cost.0, letter_i_plus);
+            transitions.iter().for_each(|trans| {
+              let new_cost = trans.score() + terr_cost.1;
+              next_tc.push((get_next_state(trans, &terr_cost.0), new_cost));
+            });      
+          });
+          next_tc.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+          seed.terrains_costs = if next_tc.len() > 50 {
+            next_tc.split_at(50).0.to_vec()  
+          } else {
+            next_tc  
+          };        
+        });
+      }  
+    }
+  }
+  look_ahead.iter_mut().for_each(|l_a| {
+    l_a.terrains_costs.sort_by(|a, b| a.1.cmp(&b.1))
+  });
+  look_ahead.sort_unstable_by(|a, b| a.terrains_costs.get(0).unwrap().1.cmp(&b.terrains_costs.get(0).unwrap().1));
+  */
+
+  let selected: &LookAhead = look_ahead.get(0).unwrap();
+  (selected.seed.to_string(), get_next_state(&selected.seed, &terrain))
+}
+
+fn get_char_index_at(i: usize, phrase: &String) -> isize {
+  let current_char: isize = phrase.chars().nth(i).unwrap().to_ascii_lowercase() as isize;
+  (current_char - (A - 1)).clamp(0, 255) // 0 to clamp spaces. 255 to avoid masking an error       
+}
+
+fn best_transitions(terrain: &Terrain, letter_i: isize) -> [Transition; POSSIBILITIES_TO_CONSIDER]{
+  let mut transitions: Vec<Transition> = terrain.runes.iter().enumerate().map(|(i, z)| {
+    Transition {
+      letter_i: letter_i as usize,
+      rune_index: i,
+      zone_move: terrain.zones.clone().set_to(i as isize, &ZONE_DOWN, &ZONE_UP),
+      rune_move: z.clone().set_to(letter_i, &RUNE_DOWN, &RUNE_UP),
+    }
+  }).collect();
+  transitions.sort_unstable_by(|a, b| a.score().cmp(&b.score()));
+  //transitions.split_at(POSSIBILITIES_TO_CONSIDER).0
+  //*transitions[..(POSSIBILITIES_TO_CONSIDER - 1)]
+  [*transitions.get(0).unwrap(), *transitions.get(1).unwrap(), *transitions.get(2).unwrap(), *transitions.get(3).unwrap()]
+}
+
+fn get_next_state(transition: &Transition, origin: &Terrain) -> Terrain {
+  let mut next_state: Terrain = Terrain {
+      runes: origin.runes,
+      zones: origin.zones,
+  };
+  next_state.zones.current = transition.rune_index as isize;
+  next_state.runes[next_state.zones.current as usize].current = transition.letter_i as isize;
+  next_state
 }
